6b713f8b082651a766f20fa309770b8f53ab442d
NuclearC <armenberug@gmail.com>
2025-10-20 22:11:37 +0400

    Stupid macro that i hate (swapchain fails on destroy idk why)

--- source/main.c
+++ source/main.c
@@ -4,7 +4,15 @@
 #include <stdlib.h>  // For setenv
 
 #include "graphics.h"
+#include "renderer.h"
 #include "window.h"
+
+#define CHECK_RESULT(x, msg)   \
+  if (0 != x) {                \
+    fprintf(stderr, msg "\n"); \
+    Cleanup();                 \
+    return -1;                 \
+  }
 
 static void Cleanup(void) {
   DestroyWindow();
@@ -16,28 +24,17 @@ int main(void) {
   int window_height = 600;
   setenv("SDL_VIDEODRIVER", "wayland", 1);
   /* initialize Vulkan */
-  if (0 != VulkanInitialize()) {
-    fprintf(stderr, "Failed to initialize Vulkan\n");
-    return -1;
-  }
-  /* create the main window */
-  if (0 != CreateWindow(window_width, window_height,
-                        "SDL3 Output Window [Vulkan]")) {
-    fprintf(stderr, "Failed to create window\n");
-    return -1;
-  }
 
-  if (0 != VulkanCreateSurface(GetWindowHandle())) {
-    fprintf(stderr, "Failed to create Vulkan surface\n");
-    Cleanup();
-    return -1;
-  }
-  /* create swapchain */
-  if (0 != VulkanCreateSwapchain(window_width, window_height)) {
-    fprintf(stderr, "Failed to create Vulkan swapchain\n");
-    Cleanup();
-    return -1;
-  }
+  CHECK_RESULT(VulkanInitialize(),
+               "Failed to initialize Vulkan instance and device");
+  /* create the main window */
+  CHECK_RESULT(
+      CreateWindow(window_width, window_height, "SDL3 Output Window [Vulkan]"),
+      "Failed to create window");
+  CHECK_RESULT(VulkanCreateSurface(GetWindowHandle()),
+               "Failed to create Vulkan surface");
+  CHECK_RESULT(VulkanCreateSwapchain(window_width, window_height),
+               "Failed to create Vulkan swapchain");
 
   /* main loop */
   for (;;) {
